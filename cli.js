9c7144919738cf1c4bff8d6792ddaf1f857f8d49
NiclasTi <niclas.titius@nicti.de>
2021-01-08 14:22:18 +0100

    Add Useragent, add verbose, add return text

--- cli.js
+++ cli.js
@@ -6,9 +6,13 @@ const version = require('./package.json').version;
 program.name('emulatedcheck').usage('[check] <options...>');
 
 program
-    .version(version, '-v, --version', 'Output the current version.')
+    .version(version, '--version', 'Output the current version.')
     .helpOption('-h, --help', 'Show this command summary.')
     .addHelpCommand(false);
+
+function increaseVerbosity(dummyValue, previous) {
+    return previous + 1;
+}
 
 program
     .command('check')
@@ -16,11 +20,17 @@ program
     .requiredOption('--url <url>','URL to check for js selector')
     .requiredOption('--selector <selector>','js selector to search for')
     .option('--count <count>','Amount of selector length equal or larger to compare','1')
-    .option('-u, --auth-username <user>', 'Basic authentication username.')
-    .option('-p, --auth-password <password>', 'Basic authentication password.')
+    .option('--user-agent <user-agent>','User agent for puppeteer to pass')
+    .option('-v, --verbose', 'verbosity that can be increased', increaseVerbosity, 0)
     .action(async (config) => {
         let result = await require('./lib/check')(config)
-        process.exit(result)
-    })
+        let verbose = config.verbose
+        if (typeof verbose === "undefined" || verbose === 1) {
+            console.log(result.v)
+        } else if (verbose && verbose >= 2) {
+            console.log(result.vv)
+        }
+        process.exit(result.code)
+    });
 
 program.parse(process.argv);
